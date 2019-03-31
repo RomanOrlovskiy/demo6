33fc1133e3bb97ecf56d0807c35f8aa19aba5f47
Roman Orlvoskyi <romanorlovskiy92@gmail.com>
2019-04-01 00:34:11 +0300

    Please dont laugh at me

--- docker-deploy-stack.sh
+++ docker-deploy-stack.sh
@@ -3,4 +3,10 @@ export SWARM_MASTER_IP=`terraform output -json swarm_masters_ips | python -c "im
 ssh -o StrictHostKeyChecking=no ubuntu@${SWARM_MASTER_IP} docker info
 scp docker-compose.yml ubuntu@${SWARM_MASTER_IP}:/home/ubuntu
 ssh ubuntu@${SWARM_MASTER_IP} docker stack deploy --compose-file=/home/ubuntu/docker-compose.yml test-stack
+
+#Please dont laugh at me for this. For some reason after first stack deploy there is no internet access from containers
+sleep 15
+ssh -o StrictHostKeyChecking=no ubuntu@${SWARM_MASTER_IP} docker stack rm test-stack
+
+ssh ubuntu@${SWARM_MASTER_IP} docker stack deploy --compose-file=/home/ubuntu/docker-compose.yml test-stack
 ssh ubuntu@${SWARM_MASTER_IP} docker service logs -f test-stack_tomcat
